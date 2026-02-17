821e5af42f83d2f507fbeb9411481df80febb0b8
fbansept <bansept.franck@gmail.com>
2026-02-17 17:31:53 +0100

    login / register

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -2,15 +2,21 @@ import { Component, inject } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [MatInputModule, MatButtonModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    MatInputModule, 
+    MatButtonModule, 
+    FormsModule, 
+    ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
   formBuilder = inject(FormBuilder);
+  httpClient = inject(HttpClient);
 
   formulaire = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
@@ -20,8 +26,10 @@ export class Login {
   onSubmitLogin() {
     console.log('tentative de connexion');
 
-    if(this.formulaire.valid){
-      console.log('envoie de la requête');
+    if (this.formulaire.valid) {
+      this.httpClient
+        .post('http://localhost:8080/login', this.formulaire.value, { responseType: 'text' })
+        .subscribe((reponse) => console.log(reponse));
     }
   }
 }
